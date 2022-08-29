63b49073ae9748b3eb8097ae2bef4f8e6cee425f
Federico <federicop@grava.io>
2022-08-29 16:46:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
