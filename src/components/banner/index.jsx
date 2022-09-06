9d7f608da15920fde7bca75d7f6016a438d850c1
pelech <pelech@fktech.net>
2022-09-06 20:09:29 -0300

    mas avances

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardTitle } from "reactstrap";
 
+import './styles.css';
+
 export const Banner = () => {
   return (
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4">Fluid jumbotron</h1>
-      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-    </div>
-  </div>
+    <Card inverse>
+      <CardImg
+        alt="Card image cap"
+        src="/assets/banner12.jpg"
+        style={{
+          height: "400px",
+          objectFit: "cover",
+          borderRadius: 0,
+        }}
+        width="100%"
+      />
+      <CardImgOverlay className="overlay">
+        <CardTitle tag="h5" className="card-title">
+          Card Title
+        </CardTitle>
+        <CardText>
+          This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+        </CardText>
+        <CardText>
+          <small className="text-muted">
+            Last updated 3 mins ago
+          </small>
+        </CardText>
+      </CardImgOverlay>
+    </Card>
   );
 };
